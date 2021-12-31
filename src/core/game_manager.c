05313d68e9bd371be018d158cdabeda229c6946b
takahatashun <takahatashunvn@gmail.com>
2021-12-31 16:36:15 +0700

    inline fn

--- src/core/game_manager.c
+++ src/core/game_manager.c
@@ -1,28 +1,28 @@
 #include "core/game_manager.h"
 
-static uint16_t getIndexLeft(Grid * grid, uint8_t x, uint8_t y) {
+static inline uint16_t getIndexLeft(Grid * grid, uint8_t x, uint8_t y) {
   return x + y * grid->size;
 }
 
-static uint16_t getIndexRight(Grid * grid, uint8_t x, uint8_t y) {
+static inline uint16_t getIndexRight(Grid * grid, uint8_t x, uint8_t y) {
   return grid->length - (x + y * grid->size);
 }
 
-static uint16_t getIndexUp(Grid * grid, uint8_t x, uint8_t y) {
+static inline uint16_t getIndexUp(Grid * grid, uint8_t x, uint8_t y) {
   return y + x * grid->size;
 }
 
-static uint16_t getIndexDown(Grid * grid, uint8_t x, uint8_t y) {
+static inline uint16_t getIndexDown(Grid * grid, uint8_t x, uint8_t y) {
   return grid->length - (y + x * grid->size);
 }
 
 typedef uint16_t (*IndexFN)(Grid *, uint8_t, uint8_t);
 
 static const IndexFN indexfn[4] = {
-  &getIndexUp,
-  &getIndexRight,
-  &getIndexDown,
-  &getIndexLeft
+  getIndexUp,
+  getIndexRight,
+  getIndexDown,
+  getIndexLeft
 };
 
 bool gm_move(Grid * grid, Direction dir) {
